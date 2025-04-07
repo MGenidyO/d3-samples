17910188c4da3f762f3bb92071a4ee950fd56c26
Mohamed Genidy <mohamed_omera2010@hotmail.com>
2025-04-07 12:35:22 +0200

    stop drag and drop and scaling

--- ProjeCS/ProjeCS.js
+++ ProjeCS/ProjeCS.js
@@ -26,9 +26,6 @@ const d3Projection = d3Identity
 
 const d3Path = d3.geoPath(d3Projection);
 
-//d3Projection.translate([0, 180,0]);
-
-//projection.rotate([x / 10, 0, 0]);
 const svg = d3
   .select("#svg_client_2_container")
   .append("svg")
@@ -40,16 +37,6 @@ const svg = d3
 
 
 const g = svg.append("g");
-
-
-// const states = g.append("g")
-//     .attr("fill", "#444")
-//     .attr("cursor", "pointer")
-//   .selectAll("path")
-//   .data(topojson.feature(us, us.objects.states).features)
-//   .join("path")
-//     .on("click", clicked)
-//     .attr("d", path);
 
 const groups = g
   .selectAll("g")
@@ -119,7 +106,11 @@ const zoom = d3.zoom()
   .scaleExtent([1, 8])
   .on("zoom", zoomed);
 
-svg.call(zoom);
+// svg.call(zoom)
+// .on("mousedown.zoom", null)
+// .on("touchstart.zoom", null)
+// .on("touchmove.zoom", null)
+// .on("touchend.zoom", null);
 
 function reset() {
   groups.transition().style("fill", null);
@@ -152,65 +143,3 @@ function zoomed(event) {
 }
 
 
-
-// function initTips() {
-//   // purge previous for dynamic render
-//   // Array.from(document.querySelectorAll('.tooltip')).forEach(el => {
-//   //   el.remove()
-//   // })
-
-//   // built upon: https://stackoverflow.com/a/69340293/10885535
-//   Array.from(document.querySelectorAll('[data-tip]')).forEach(el => {
-//     // tip
-//     let tip = document.createElement('div')
-//     tip.classList.add('tooltip')
-//     //tip.innerText = el.getAttribute('data-tip')
-//     tip.style.backgroundColor = '#E5E4E2';
-//     tip.style.border = '1.8px solid #8B8000';
-//     tip.style.color = '#041E42';
-//     tip.style.fontSize = '13px';
-//     tip.style.fontFamily = `'Montserrat', 'Proxima Nova', 'Gotham', 'Avenir', sans-serif`;
-//     tip.style.letterSpacing = '1px';
-//     tip.style.fontWeight = '500';
-//     tip.style.width = '230px'
-
-//     document.body.appendChild(tip)
-//     // logo
-//     let logo = document.createElement('img')
-//     logo.src = "../images/projecs2.png";
-
-//     tip.appendChild(logo)
-
-//     let divInfo = document.createElement('div')
-//     divInfo.innerHTML = `<p></p><p><b>ZONE:</b> ${el.__data__.properties.Zone}</p>
-//       <p><b>AREA:</b> ${el.__data__.properties.AREA}</p>
-//       <p><b>LAYER:</b> ${el.__data__.properties.LAYER}</p>
-//       <p><b>DESCRIPTION:</b>  ${client_data.find((e) => e.RoomID == el.__data__.properties.LAYER)?.RoomDescription}</p><p></p>`
-//     tip.appendChild(divInfo)
-//     //assets.attr("data-tip", d => `Zone: ${d.properties.Zone}\nAREA: ${d.properties.AREA}\nLAYER: ${d.properties.LAYER}\nDescription: ${client_data.find((e) => e.RoomID == d.properties.LAYER)?.RoomDescription }`)
-
-//     // arrow
-//     let arrow = document.createElement('div')
-//     arrow.classList.add('tooltip-arrow')
-//     tip.appendChild(arrow)
-
-//     // toggle with mouse
-//     el.onmouseover = e => {
-//       tip.style.opacity = 1
-//       tip.style.visibility = 'visible'
-//       let elmPos = el.getBoundingClientRect()
-//       let tipPos = tip.getBoundingClientRect()
-//       tip.style.left = (elmPos.left + (elmPos.width - tipPos.width) / 2) + 'px'
-//       tip.style.top = (elmPos.top + (elmPos.height)) + 'px'
-//       arrow.style.left = (tipPos.width / 2 - 5) + 'px'
-//       e.stopPropagation() // stop parent
-//     };
-//     el.onmouseout = e => {
-//       tip.style.opacity = 0
-//       tip.style.visibility = 'hidden'
-//     };
-//   });
-// }
-
-// kickoff
-//initTips()
